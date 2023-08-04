6de490e6653f528706e8c95b87c211c028f7b52b
CayseCollins <codecravt@gmail.com>
2023-08-03 20:35:09 -0600

    Add Research folder

---
+++ src/Week04/wk4Research.java
@@ -0,0 +1,97 @@
+package Week04;
+
+public class wk4Research {
+
+	public static void main(String[] args) {
+//		Prompts
+//
+// Strings
+//
+//		1. Select five (5) methods from the String Java Documentation and describe the following for each: 
+//			1 - indexOf
+//			2 - charAt
+//			3 - concat
+//			4 - replace
+//			5 - toLowerCase
+//	
+//			(a) What is the method signature for each method?
+//				1 - public int indexOf(int, int)
+//				2 - public char cahrAt(int index)
+//				3 - public String concat(String str) (Concatenation is when a string is made by adding a specific string to the end of itself or another string)
+//				4 - public String replace(char oldChar, char newChar)
+//				5 - public String toLowerCase()
+//		
+//		
+//			(b) What does each method do? 
+//				1 - Returns the first occurrence of the character (or -1 if not occurring) inside the specified string. 
+//				2 - Returns the value of char at the chosen index point. (Always start with 0 for index point, or your return will be off by 1)
+//				3 - Returns the a new string by adding specified string(s) to the end of another string. (Or 0 which returns the original String object)
+//				4 - Returns a new string where the oldChar has been replaced by the newChar, but is otherwise the same as the original String. 
+//				5 - Returns the specified String to all lower case. (Is a locale-sensitive operation, may need to use .ROOT when performing for a specific locale)
+//		
+//		
+//			(c) Why would this method be useful (how could you use it)?  Give an example.
+//				1 - Useful if the user needs to view information stored at a specific point in an index, without viewing the entire index all at once.
+//					Ex. User wants to view the number of a specific player on a baseball team roster. Will search for that player's name and return the number.   
+//				2 - Useful when the need to discover if a specific character or substring is present in an index without pulling up the entire index.
+//					Ex. User wants to discover how many times the word LIKE is used in a specific string such as a paragraph or entire book. 
+//				3 - Useful when tying to Strings together.
+//					Ex. 
+//				4 - Useful if the user needs to replace something in the index and does not want to go through each one individually. 
+//					Ex. User spelled a DND character's name incorrectly and needs to replace it throughout the web site.  
+//				5 - Useful when taking user input and making it adhere to case sensitivity that may exist. 
+//					Ex. 
+//	
+//	Sources: 
+//		https://www.crio.do/blog/string-methods-in-java/
+//		https://docs.oracle.com/en/java/javase/17/docs/api/java.base/java/lang/String.html
+//		https://www.oreilly.com/library/view/actionscript-the-definitive/1565928520/re236.html#:~:text=The%20toLowerCase(%20)%20method%20creates%20a,facilitate%20case%2Dinsensitive%20character%20comparisons.
+//		https://www.geeksforgeeks.org/applications-of-string-indexof-method-in-java/
+//		
+//	Arrays
+//		2. Select five (5)  methods from the Array Java Documentation and describe the following for each: 
+//			1 - get
+//			2 - getLength
+//			3 - set
+//			4 - setChar
+//			5 - setInt
+//		
+//			(a) What is the method signature for each method?
+//				1 - public static Object get(Object array, int index)
+//				2 - public static int getLength(Object array)
+//				3 - public static void set(Object array, int index, Object value)
+//				4 - public static void setChar(Object array, int index, char c)
+//				5 - public static void setInt(Object array,int index,int i)
+//			
+//			(b) What does each method do? 
+//				1 - 
+//				2 - 
+//				3 - 
+//				4 - 
+//				5 - 
+//			
+//			(c) Why would this method be useful (how could you use it)? Give an example.
+//				1 - 
+//				2 - 
+//				3 - 
+//				4 - 
+//				5 - 
+//	
+//		
+//	Source(s):
+//		https://docs.oracle.com/en/java/javase/17/docs/api/java.base/java/lang/reflect/Array.html
+//		https://www.linkedin.com/pulse/mastering-javascript-arrays-comprehensive-guide-essential-sharma/
+//
+//
+//	Instructions
+//	Choose at least two (2) of the above prompts.
+//	Research those prompts.
+//	Write at least one paragraph for two (2) of the above prompts
+//	Include URLs from where you found the information to cite your sources. 
+//	Submit your Research Results to the LMS.
+//
+
+		
+	}
+
+}
---
+++ src/Week05/wk5Research.java
@@ -0,0 +1,35 @@
+package Week05;
+
+public class wk5Research {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+/*		Prompts
+
+	5. What is an exception?
+	
+	A: 
+
+
+	6. What are the differences between checked and unchecked exceptions?
+	A:
+	
+	
+	Source(s):
+
+		7. What is unit testing and why is it important?
+	A: 
+
+	
+	Source(s):
+
+	Instructions
+	Choose at least two (2) of the above prompts.
+	Research those prompts.
+	Write at least one paragraph for two (2) of the above prompts
+	Include URLs from where you found the information to cite your sources. 
+	Submit your Research Results to the LMS.
+*/
+	}
+
+}
